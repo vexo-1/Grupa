a2c25015ee91235d2318abae381a32bdd87bf3df
Bartek <bartus2108@gmail.com>
2025-12-03 23:48:23 +0100

    Komentarze JS
    Żebyśmy potem wiedzieli jak to działa

--- JS/theme.js
+++ JS/theme.js
@@ -1,18 +1,19 @@
-const body = document.body;
+const body = document.body; //Reference to the HTML body
 const dark = "#1E1E1E";
 const light = "#e7e7e7";
 
-let isDark = localStorage.getItem("isDark") === "true"; // Stets isDark as true (Equal value and equal type)
-body.style.backgroundColor = isDark ? dark : light; // Checkes if isDark is light or dark (Short for if-else)
+let isDark = localStorage.getItem("isDark") === "true";
+// Checkes if user previously saved dark mode in browser storage (Equal value and equal type)
+body.style.backgroundColor = isDark ? dark : light; // Sets background color (if true uses dark mode) (Short for if-else)
 
 function theme() {
     if (isDark) {
-        body.style.backgroundColor = light;
-        isDark = false;
-        localStorage.setItem("isDark", "false");
+        body.style.backgroundColor = light; // Changes the background color of the page
+        isDark = false; // Updates isDark
+        localStorage.setItem("isDark", "false"); // Saves theme in local storage
     } else {
-        body.style.backgroundColor = dark;
-        isDark = true;
-        localStorage.setItem("isDark", "true");
+        body.style.backgroundColor = dark; // Changes the background color of the page
+        isDark = true; // Updates isDark
+        localStorage.setItem("isDark", "true"); // Saves theme in local storage
     }
 }
