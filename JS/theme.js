a30237caa2842d76048b7c70fad9496d68ab3d5a
Bartek <bartus2108@gmail.com>
2025-12-03 23:26:00 +0100

    JS - początek
    dodałem przycisk zmieniający motyw z jasnego na ciemny

--- JS/theme.js
+++ JS/theme.js
@@ -1,18 +1,18 @@
-const body = document.body
-const dark = "#1E1E1E"
-const light = "#FFFF"
+const body = document.body;
+const dark = "#1E1E1E";
+const light = "#e7e7e7";
+
+let isDark = localStorage.getItem("isDark") === "true"; // Stets isDark as true (Equal value and equal type)
+body.style.backgroundColor = isDark ? dark : light; // Checkes if isDark is light or dark (Short for if-else)
 
 function theme() {
-    localStorage.removeItem("isDark")
-localStorage.setItem("isDark", "false")
-    let isDark = localStorage.getItem("isDark")
-    console.log(isDark)
-    localStorage.setItem("isDark", "true")
     if (isDark) {
-        body.style.backgroundColor = light
-        localStorage.setItem("isDark", "false")
-    } else{
-        body.style.backgroundColor = dark
-        localStorage.setItem("isDark", "true")
+        body.style.backgroundColor = light;
+        isDark = false;
+        localStorage.setItem("isDark", "false");
+    } else {
+        body.style.backgroundColor = dark;
+        isDark = true;
+        localStorage.setItem("isDark", "true");
     }
 }
