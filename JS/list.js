53700180ea7ff52f5de09bf7d7cda0eb8d7e8e3e
Bartek <247160529+bartus2108-cell@users.noreply.github.com>
2025-12-18 09:53:46 +0100

    Zapisywanie listy po odświerzeniu

--- JS/list.js
+++ JS/list.js
@@ -1,9 +1,29 @@
+if (localStorage.getItem("lista") == null){
+    let lista = ["Butelka", "Talerz"]
+    localStorage.setItem("lista", JSON.stringify(lista))
+}
+let lista1 = JSON.parse(localStorage.getItem("lista"))
+let li
+for (let i = 0; i < lista1.length; i++){
+    li = document.createElement("li");
+    li.innerText = lista1[i]
+    document.getElementById("list").appendChild(li);
+}
 document.getElementById("li1").onclick = function() { // Funkcja wywołana gdy przyciśnięty przycisk z id "li1"
     let li1 = prompt("Dodaj rzecz do listy"); // Zmienna tworząca prompt do wpisania elementu do listy
     if (li1 !== null && li1 !== "") { // Warunek sprawdzający czy urzytkownik wpisał coś w prompt
-        let li = document.createElement("li"); // 
+        li = document.createElement("li"); // 
         let li1Node = document.createTextNode(li1); // 
         li.appendChild(li1Node); // 
-        document.getElementById("list").appendChild(li); // 
+        let but = document.createElement("button")
+        but.innerText = "Usuń"
+        but.onclick = usuwanie()
+        li.appendChild(but)
+        document.getElementById("list").appendChild(li); //
+        lista1.push(li1)
+        localStorage.setItem("lista", JSON.stringify(lista1))
     }
+}
+function usuwanie(){
+
 }
